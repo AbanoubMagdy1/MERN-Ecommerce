34b903a6faf8c0bed66832d97b88a74b04c0f5ed
AbanoubMagdy1 <bebomagdy666@gmail.com>
2020-10-08 17:22:09 +0200

    Add error handling to backend and install redux

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,18 +1,30 @@
 import express from 'express';
 import Product from '../models/Product.js';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const products = await Product.find({}).select(
-    '_id name rating numReviews price image'
-  );
-  res.send(products);
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({}).select(
+      '_id name rating numReviews price image'
+    );
+    res.send(products);
+  })
+);
 
-router.get('/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.send(product);
-});
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404);
+      throw new Error('Product not found');
+    }
+  })
+);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ dotenv.config();
 import express from 'express';
 import confifDB from './services/db.js';
 import colors from 'colors';
-
+import { notFound, errorHandler } from './middleware/errorHandling.js';
 import productsRoutes from './routes/products.js';
 
 confifDB();
@@ -12,5 +12,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use('/api/products', productsRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, console.log(`Server is running at ${port}`.green.bold));
