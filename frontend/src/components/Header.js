90132456ef25f2669e04ac5240d2776423b0e2e0
AbanoubMagdy1 <bebomagdy666@gmail.com>
2021-03-13 18:21:56 +0200

    Prepare for deployment

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>ProShop</Navbar.Brand>
+            <Navbar.Brand>Shoppy</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
