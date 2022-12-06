a21ac417b00d087c74c94e895d49e3db0131e009
AbanoubMagdy1 <bebomagdy666@gmail.com>
2022-12-06 18:05:29 +0200

    Add build frontend

---
+++ frontend/build/precache-manifest.63ce8cfd3647cca3d37f9df46013d0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a101c3d16e16013ab6b029461099dbfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "441499a2626837eebc81",
+    "url": "/static/css/main.6280d12f.chunk.css"
+  },
+  {
+    "revision": "f74b9ddef1e1b97939c7",
+    "url": "/static/js/2.00ea027d.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.00ea027d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441499a2626837eebc81",
+    "url": "/static/js/main.d98a5d15.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
